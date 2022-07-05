d57287a68d44cc7178ea2a12875f44c93bcf78fc
Yan Voronkov <sub.voronkov@gmail.com>
2022-07-05 17:04:27 +0300

    update

--- js/script.js
+++ js/script.js
@@ -46,11 +46,13 @@ if (isMobile.any()) {
 
 const iconMenu = document.querySelector('.menu__icon');
 const menuBody = document.querySelector('.menu__body');
+const menuList = document.querySelector('.menu__list');
 if (iconMenu) {
 	iconMenu.addEventListener("click", function (e) {
 		document.body.classList.toggle('_lock')
 		iconMenu.classList.toggle('_active');
 		menuBody.classList.toggle('_active');
+		menuList.classList.toggle('_active');
 	});
 }
 
